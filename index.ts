a6365dd836b06220fdf739c9ba6d633c7774ed56
Samiya142 <samiya2011099@gmail.com>
2024-03-26 13:58:39 +0500

    Pushimg my simple calculator on GitHub for the fist time

---
+++ index.ts
@@ -0,0 +1,40 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+// Asking Queation from users through inquirer
+
+const answer = await inquirer.prompt([
+  { message: "Enter First Number", type: "number", name: "firstNumber" },
+  { message: "Enter Second Number", type: "number", name: "secondNumber" },
+  {
+    message: "Select one operator to perform Operations",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division", "Modulus", "Exponent"],
+  },
+]);
+
+// Conditional Statements If.else
+
+if(answer.operator === "Addition"){
+    console.log(answer.firstNumber + answer.secondNumber)
+}
+else if(answer.operator === "Subtraction"){
+    console.log(answer.firstNumber - answer.secondNumber)
+}
+else if(answer.operator === "Multiplication"){
+    console.log(answer.firstNumber * answer.secondNumber)
+}
+else if(answer.operator === "Division"){
+    console.log(answer.firstNumber / answer.secondNumber)
+}
+else if(answer.operator === "Modulus"){
+    console.log(answer.firstNumber % answer.secondNumber)
+}
+else if(answer.operator === "Exponent"){
+    console.log(answer.firstNumber ** answer.secondNumber)
+}
+else{
+    console.log("Invalid Input")
+}
